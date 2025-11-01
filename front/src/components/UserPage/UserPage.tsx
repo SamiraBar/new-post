61dffcbd3bbe23e18137201cffae658b1616c5c3
Sadykov Arsen <LoCo10291998@gmail.com>
2025-11-01 17:23:54 +0600

    Downloaded libs for avatar.

--- front/src/components/UserPage/UserPage.tsx
+++ front/src/components/UserPage/UserPage.tsx
@@ -1,9 +1,10 @@
 import InfoSendPost from "@/features/InfoSendPost.tsx";
+import UserToolbar from "@/components/UserToolbar/UserToolbar.tsx";
 
 const UserPage = () => {
     return (
         <div>
-            <header><h1>Добро пожаловать!</h1></header>
+            <header><h1><UserToolbar/></h1></header>
             <main>
                 <div style={{ color: '#666' }}>
                     Расчитать/Следить за посылкой тут.
