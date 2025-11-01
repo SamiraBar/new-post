665f049bd7b0c8e0c2f97863e4a472b177aef9cf
Dilnoza Tasheva <dilnozatasheva@gmail.com>
2025-11-01 19:06:51 +0100

    Added footer component with responsive design and social network icons

--- front/src/components/UserPage/UserPage.tsx
+++ front/src/components/UserPage/UserPage.tsx
@@ -1,16 +1,20 @@
 import InfoSendPost from "@/features/InfoSendPost.tsx";
 import UserToolbar from "@/components/UserToolbar/UserToolbar.tsx";
+import FooterComponent from "@/features/FooterComponent.tsx";
 
 const UserPage = () => {
     return (
         <div>
             <header><h1><UserToolbar/></h1></header>
             <main>
-                <div style={{ color: '#666' }}>
+                <div style={{color: '#666'}}>
                     Расчитать/Следить за посылкой тут.
                 </div>
                 <div className={'pl-2'}>
                     <InfoSendPost/>
+                </div>
+                <div className={'pl-2'}>
+                    <FooterComponent/>
                 </div>
             </main>
         </div>
