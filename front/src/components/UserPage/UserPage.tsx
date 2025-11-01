aefb805e0140e86788202ff3c9814601a6c3db7f
Musulmankul <mussangle1@gmail.com>
2025-11-01 15:07:56 +0600

    Added UserToolbar component, changed body styles, created container styles in index.css

--- front/src/components/UserPage/UserPage.tsx
+++ front/src/components/UserPage/UserPage.tsx
@@ -1,12 +1,11 @@
+import UserToolbar from "../UserToolbar/UserToolbar";
+
 const UserPage = () => {
-    return (
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <h1>Добро пожаловать!</h1>
-            <p style={{ color: '#666' }}>
-                Это контент для обычного пользователя.
-            </p>
-        </div>
-    );
+  return (
+    <div>
+      <UserToolbar />
+    </div>
+  );
 };
 
 export default UserPage;
