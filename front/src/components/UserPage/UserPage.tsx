9dfae1d055b9ffe134ae49c494a6d21d3704c26a
Akylbek <hsplay74@gmail.com>
2025-11-02 18:49:53 +0600

    feat(delivery): update component added icons and warning cards Add DeliveryCostCalculator to UserPage

--- front/src/components/UserPage/UserPage.tsx
+++ front/src/components/UserPage/UserPage.tsx
@@ -1,24 +1,28 @@
-import InfoSendPost from "@/features/infoSendPost/InfoSendPost.tsx";
-import UserToolbar from "@/components/UserToolbar/UserToolbar.tsx";
-import FooterComponent from "@/features/footer/FooterComponent.tsx";
+import InfoSendPost from '@/features/infoSendPost/InfoSendPost.tsx';
+import UserToolbar from '@/components/UserToolbar/UserToolbar.tsx';
+import FooterComponent from '@/features/footer/FooterComponent.tsx';
+import DeliveryCostCalculator from '@/features/DeliveryCostCalculator.tsx';
 
 const UserPage = () => {
-    return (
-        <div>
-            <header><h1><UserToolbar/></h1></header>
-            <main>
-                <div style={{color: '#666'}} className={'mx-auto my-10 max-w-6xl'}>
-                    Расчитать/Следить за посылкой тут.
-                </div>
-                <div className={'pl-2'}>
-                    <InfoSendPost/>
-                </div>
-                <div className={'pl-2'}>
-                    <FooterComponent/>
-                </div>
-            </main>
+  return (
+    <div>
+      <header><h1><UserToolbar/></h1></header>
+      <main>
+        <div style={{color: '#666'}} className={'mx-auto my-10 max-w-6xl'}>
+          Расчитать/Следить за посылкой тут.
         </div>
-    );
+        <div className={'pl-2'}>
+          <InfoSendPost/>
+        </div>
+        <div className={'pl-2'}>
+          <DeliveryCostCalculator/>
+        </div>
+        <div className={'pl-2'}>
+          <FooterComponent/>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default UserPage;
