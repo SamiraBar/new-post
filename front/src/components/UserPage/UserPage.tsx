cee7d5687cefcd61c8c71f64649d0a5398913688
Akylbek <hsplay74@gmail.com>
2025-11-02 21:06:23 +0600

    feat(user-page): add new company info and delivery calculating components

--- front/src/components/UserPage/UserPage.tsx
+++ front/src/components/UserPage/UserPage.tsx
@@ -2,6 +2,9 @@ import InfoSendPost from "@/features/infoSendPost/InfoSendPost.tsx";
 import UserToolbar from "@/components/UserToolbar/UserToolbar.tsx";
 import FooterComponent from "@/features/footer/FooterComponent.tsx";
 import DeliveryCostCalculator from '@/features/deliveryCostCalculator/DeliveryCostCalculator.tsx';
+import DeliveryCalculating from '@/features/DeliveryCalculating/DeliveryCalculating.tsx';
+import ImportantInfo from '@/features/company-info/ImportantInfo.tsx';
+import AboutCompany from '@/features/company-info/AboutCompany.tsx';
 
 const UserPage = () => {
     return (
