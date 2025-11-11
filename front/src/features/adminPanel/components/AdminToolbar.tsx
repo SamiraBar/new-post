65b8ec2333f9c917a92f0f880e0a065ef177d30a
Sadykov Arsen <LoCo10291998@gmail.com>
2025-11-11 21:57:48 +0600

    Updated favicon and document title Fixed WhatsApp button in user toolbar Fix WhatsApp and Instagram links in footer Rename feature folders  in frontend to use camelCase

--- front/src/features/adminPanel/components/AdminToolbar.tsx
+++ front/src/features/adminPanel/components/AdminToolbar.tsx
@@ -9,7 +9,7 @@ import { Button } from '@/components/ui/button.tsx';
 import { type ChangeEvent, useState } from 'react';
 import logoImage from '@/assets/logo/newPostLogo.jpeg';
 import useAdminStore from '@/stores/adminStore/adminStore.ts';
-import ModalFile from '@/features/AdminPanel/components/ModalFile.tsx';
+import ModalFile from '@/features/adminPanel/components/ModalFile.tsx';
 
 export const AdminToolbar = () => {
   const { logout } = useAdminStore();
