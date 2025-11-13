a727ae44064edbdf34911c2643697db157d05a4d
Sadykov Arsen <LoCo10291998@gmail.com>
2025-11-13 19:42:06 +0600

    Added package weight checks. Must be a positive number, greater than 0.1 kg and no more than 15 kg. Deleted unusual constants in constants.ts

--- front/src/constants.ts
+++ front/src/constants.ts
@@ -2,15 +2,6 @@ export const API_URL = 'http://localhost:8000';
 
 export const cities = ['Bishkek', 'Osh', 'Karakol', 'Naryn'];
 
-export const BASE_PRICE = 600;
-
-export const tariffs = [
-  { maxWeight: 3, pricePerKg: 125 },
-  { maxWeight: 6, pricePerKg: 135 },
-  { maxWeight: 12, pricePerKg: 140 },
-  { maxWeight: 15, pricePerKg: 145 },
-];
-
 export const offices = [
   { id: 1, name: 'Офис Бишкек - Центр', address: 'ул. Чуй 100' },
   { id: 2, name: 'Офис Бишкек - Восток', address: 'мкр. Восток-5' },
