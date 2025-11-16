0eb2edf53406396c400420f5a6f8b74bedd4965b
daniella-bgchva <bogachevadaniella@gmail.com>
2025-11-16 15:00:34 +0600

    Cleaned up the logic.

--- back/types.d.ts
+++ back/types.d.ts
@@ -6,12 +6,3 @@ export interface AdminDef {
   displayName: string;
 }
 
-export interface CourierCityRow {
-    "Название города": string;
-    "Страна"?: string;
-}
-
-export interface PickupCityRow {
-    "Название города": string;
-    "Регион"?: string;
-}
