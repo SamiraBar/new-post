0c1de138a09bc8fa3a20ba872ad7a303ae41b5fc
Akylbek <hsplay74@gmail.com>
2025-11-26 15:21:30 +0600

    feat(admin): add `adminEdit` endpoint and fix code formatting in admin controllers
    - Introduce `adminEdit` endpoint to allow admin modifications.
    - Update imports and enforce consistent code formatting in controllers and routes for better readability.

--- back/controllers/admin.ts
+++ back/controllers/admin.ts
@@ -1,29 +1,29 @@
-import Admin from "../models/Admin";
-import {NextFunction, Request, Response} from "express";
-import {AdminDef} from "../types";
-import mongoose from "mongoose";
+import Admin from '../models/Admin';
+import { NextFunction, Request, Response } from 'express';
+import { AdminDef } from '../types';
+import mongoose from 'mongoose';
 
 export const adminLogin = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const admin = await Admin.findOne({email: req.body.email});
 
-    if (!admin) return res.status(400).send({error: "Invalid login"});
+    if (!admin) return res.status(400).send({error: 'Invalid login'});
 
-    const isMatch = await admin.checkPassword(req.body.password)
+    const isMatch = await admin.checkPassword(req.body.password);
 
-    if (!isMatch) return res.status(400).send({error: "Wrong password"});
+    if (!isMatch) return res.status(400).send({error: 'Wrong password'});
 
     admin.generateToken();
     await admin.save();
 
     res.send(admin);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 export const adminCreate = async (req: Request, res: Response, next: NextFunction) => {
-  const adminData: Omit<AdminDef, "token" | "role"> = {
+  const adminData: Omit<AdminDef, 'token' | 'role'> = {
     email: req.body.email,
     password: req.body.password,
     displayName: req.body.displayName,
@@ -48,31 +48,31 @@ export const adminCreate = async (req: Request, res: Response, next: NextFunctio
 
     next(error);
   }
-}
+};
 
 export const adminDelete = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const removeUser = await Admin.findById(req.params.id);
 
-    if (!removeUser) return res.status(404).send({error: "User is not found"});
+    if (!removeUser) return res.status(404).send({error: 'User is not found'});
 
-    if (removeUser.role === "superAdmin") return res.status(400).send({error: "Cannot delete super admin"});
+    if (removeUser.role === 'superAdmin') return res.status(400).send({error: 'Cannot delete super admin'});
 
-    await Admin.findByIdAndDelete(req.params.id)
-    res.send({message: "User is deleted"});
+    await Admin.findByIdAndDelete(req.params.id);
+    res.send({message: 'User is deleted'});
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 export const allAdmins = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const admins = await Admin.find().sort({role: -1});
     res.send(admins);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 export const adminLogout = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -91,4 +91,24 @@ export const adminLogout = async (req: Request, res: Response, next: NextFunctio
   } catch (error) {
     next(error);
   }
-}
+};
+
+export const adminEdit = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const id = req.params.id;
+    const {
+      displayName,
+      email,
+      password
+    } = req.body;
+    const admin = await Admin.findById(id);
+    if (!admin) return res.status(404).send({error: 'User is not found'});
+    if (displayName) admin.displayName = displayName;
+    if (email) admin.email = email;
+    if (password) admin.password = password;
+    await admin.save();
+    res.status(200).send(admin);
+  } catch (error) {
+    next(error);
+  }
+};
