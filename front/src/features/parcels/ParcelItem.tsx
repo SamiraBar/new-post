b3cf214f7a06442152d4d32450475d6aae878b7c
Musulmankul <mussangle1@gmail.com>
2025-11-26 16:11:14 +0600

    fixed parlcels styles

--- front/src/features/parcels/ParcelItem.tsx
+++ front/src/features/parcels/ParcelItem.tsx
@@ -1,7 +1,8 @@
 import type { IParcel } from '@/types';
 import {
   Barcode,
-  Building2, Check,
+  Building2,
+  Check,
   CheckCircle2,
   Ellipsis,
   Map,
@@ -10,7 +11,7 @@ import {
   Package,
   Play,
   StickyNote,
-  Truck
+  Truck,
 } from 'lucide-react';
 import dayjs from 'dayjs';
 
@@ -23,45 +24,44 @@ import { useNavigate } from 'react-router-dom';
 
 const steps = [
   {
-    icon: <FileText size={25}/>,
+    icon: <FileText size={25} />,
     statusValue: 'draft',
   },
   {
-    icon: <Package size={25} className="text-blue-500"/>,
+    icon: <Package size={25} className="text-blue-500" />,
     statusValue: 'created',
   },
   {
-    icon: <CheckCircle2 size={25} className="text-fuchsia-500"/>,
+    icon: <CheckCircle2 size={25} className="text-fuchsia-500" />,
     statusValue: 'accepted',
   },
   {
-    icon: <Truck size={25} className="text-gray-400"/>,
+    icon: <Truck size={25} className="text-gray-400" />,
     statusValue: 'shipped',
   },
   {
-    icon: <Map size={25} className="text-orange-700"/>,
+    icon: <Map size={25} className="text-orange-700" />,
     statusValue: 'in_country',
   },
   {
-    icon: <Building2 size={25} className="text-orange-500"/>,
+    icon: <Building2 size={25} className="text-orange-500" />,
     statusValue: 'in_city',
   },
   {
-    icon: <MapPin size={25} className="text-lime-600"/>,
+    icon: <MapPin size={25} className="text-lime-600" />,
     statusValue: 'at_pickup_point',
   },
   {
-    icon: <Check size={25} className="text-green-600"/>,
+    icon: <Check size={25} className="text-green-600" />,
     statusValue: 'delivered',
   },
 ];
-
 
 interface Props {
   parcel: IParcel;
 }
 
-const ParcelItem = ({parcel}: Props) => {
+const ParcelItem = ({ parcel }: Props) => {
   const {
     _id,
     trackingNumber,
@@ -78,115 +78,113 @@ const ParcelItem = ({parcel}: Props) => {
   const navigate = useNavigate();
 
   return (
-      <div className="flex flex-col lg:flex-row gap-4 w-full bg-amber-50 p-4 md:p-6 rounded-2xl relative">
-        <div className="absolute top-4 right-4 lg:top-6 lg:right-6">
-          <div
-              className="size-4 md:w-20 md:h-20 rounded-full border-4 border-orange-500  flex items-center justify-center">
-            {steps.find(step => step.statusValue === status)?.icon}
-          </div>
+    <div className="flex flex-col lg:flex-row gap-4 w-full bg-amber-50 p-4 md:p-6 rounded-2xl relative">
+      <div className="absolute top-4 right-4 lg:top-6 lg:right-6">
+        <div className="size-4 w-20 h-20 rounded-full border-4 border-orange-500  flex items-center justify-center">
+          {steps.find((step) => step.statusValue === status)?.icon}
         </div>
+      </div>
 
-        <div className="flex-1 pr-20 lg:pr-24">
-          <div className="flex flex-wrap items-center gap-3 mb-4">
+      <div className="flex-1 pr-20 lg:pr-24">
+        <div className="flex flex-wrap items-center gap-3 mb-4">
           <span className="text-gray-700 bg-gray-300 py-1 px-3 rounded-2xl text-xs md:text-sm font-medium">
             KG312-1
           </span>
-            <div className="flex gap-1">
-              <Play size={12} fill="currentColor"/>
-              <Play size={12} fill="currentColor"/>
+          <div className="flex gap-1">
+            <Play size={12} fill="currentColor" />
+            <Play size={12} fill="currentColor" />
+          </div>
+          <p className="font-bold m-0 text-base md:text-lg">{trackingNumber}</p>
+          <div className="flex md:flex-row md:items-center gap-3 md:ml-auto sm: flex-col  sm: items-start">
+            <p className="font-bold m-0 text-base md:text-lg">{partnerTrackingNumber}</p>
+            <span className="text-gray-500 text-sm">(трек номер партнера)</span>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-12 gap-4 items-center">
+            <div className="flex gap-3 md:col-span-2">
+              <img src={icCalendar} alt="Calendar icon" className="w-5 h-5" />
+              <div className="flex flex-col gap-0.5 text-sm ">
+                <span className="font-medium">{dayjs(draftedAt).format('DD.MM.YYYY')}</span>
+                <span className="text-gray-600">{dayjs(draftedAt).format('HH:mm:ss')}</span>
+              </div>
             </div>
-            <p className="font-bold m-0 text-base md:text-lg">{trackingNumber}</p>
-            <div className="flex md:flex-row md:items-center gap-3 md:ml-auto sm: flex-col  sm: items-start">
-              <p className="font-bold m-0 text-base md:text-lg">{partnerTrackingNumber}</p>
-              <span className="text-gray-500 text-sm">(трек номер партнера)</span>
+
+            <div className="flex gap-3 md:col-span-5 md:flex-row sm: flex-col">
+              <p className="m-0 text-base whitespace-nowrap">Отправитель:</p>
+              <div className="flex items-center gap-2">
+                <img src={icUser} alt="User icon" className="w-5 h-5" />
+                <p className="m-0 font-bold text-base">{sender.fullName}</p>
+              </div>
+            </div>
+
+            <div className="flex justify-between md:justify-end gap-4 md:col-span-5 md:flex-row sm: flex-col lg:items-center">
+              {isPaid && (
+                <div className="flex items-center gap-2">
+                  <img src={icBanknote} alt="Banknote icon" className="w-8 h-8 md:w-9 md:h-9" />
+
+                  {partnerStickerReceived && (
+                    <a
+                      href="#"
+                      className="p-2 rounded-lg hover:bg-amber-100 transition-colors hover:scale-110 duration-300"
+                    >
+                      <StickyNote color="#6b6b6b" strokeWidth={3} size={24} />
+                    </a>
+                  )}
+
+                  <a
+                    href="#"
+                    className="p-2 rounded-lg hover:bg-amber-100 transition-colors hover:scale-110 duration-300"
+                  >
+                    <Barcode color="#6b6b6b" strokeWidth={3} size={24} />
+                  </a>
+                </div>
+              )}
+
+              <div className="flex gap-2 items-center">
+                <img src={icWeight} alt="Weight icon" className="w-5 h-5" />
+                <p className="m-0 text-base md:text-lg font-bold whitespace-nowrap">
+                  Вес: {weight} кг
+                </p>
+              </div>
             </div>
           </div>
 
-          <div className="flex flex-col gap-4">
-            <div className="grid grid-cols-1 md:grid-cols-12 gap-4 items-center">
-              <div className="flex gap-3 md:col-span-2">
-                <img src={icCalendar} alt="Calendar icon" className="w-5 h-5"/>
-                <div className="flex flex-col gap-0.5 text-sm ">
-                  <span className="font-medium">{dayjs(draftedAt).format('DD.MM.YYYY')}</span>
-                  <span className="text-gray-600">{dayjs(draftedAt).format('HH:mm:ss')}</span>
-                </div>
-              </div>
+          <div className="grid grid-cols-1 md:grid-cols-12 gap-4 items-center">
+            <div className="hidden md:block md:col-span-2"></div>
 
-              <div className="flex gap-3 md:col-span-5 md:flex-row sm: flex-col">
-                <p className="m-0 text-base whitespace-nowrap">Отправитель:</p>
-                <div className="flex items-center gap-2">
-                  <img src={icUser} alt="User icon" className="w-5 h-5"/>
-                  <p className="m-0 font-bold text-base">{sender.fullName}</p>
-                </div>
-              </div>
-
-              <div
-                  className="flex justify-between md:justify-end gap-4 md:col-span-5 md:flex-row sm: flex-col lg:items-center">
-                {isPaid && (
-                    <div className="flex items-center gap-2">
-                      <img src={icBanknote} alt="Banknote icon" className="w-8 h-8 md:w-9 md:h-9"/>
-
-                      {partnerStickerReceived && (
-                          <a
-                              href="#"
-                              className="p-2 rounded-lg hover:bg-amber-100 transition-colors hover:scale-110 duration-300"
-                          >
-                            <StickyNote color="#6b6b6b" strokeWidth={3} size={24}/>
-                          </a>
-                      )}
-
-                      <a
-                          href="#"
-                          className="p-2 rounded-lg hover:bg-amber-100 transition-colors hover:scale-110 duration-300"
-                      >
-                        <Barcode color="#6b6b6b" strokeWidth={3} size={24}/>
-                      </a>
-                    </div>
-                )}
-
-                <div className="flex gap-2 items-center">
-                  <img src={icWeight} alt="Weight icon" className="w-5 h-5"/>
-                  <p className="m-0 text-base md:text-lg font-bold whitespace-nowrap">
-                    Вес: {weight} кг
-                  </p>
-                </div>
+            <div className="flex gap-3 md:col-span-5 md:flex-row sm: flex-col">
+              <p className="m-0 text-base whitespace-nowrap">Получатель:</p>
+              <div className="flex items-center gap-2 lg:pl-2.5">
+                <img src={icUser} alt="User icon" className="w-5 h-5" />
+                <p className="m-0 font-bold text-base">{recipient.fullName}</p>
               </div>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-12 gap-4 items-center">
-              <div className="hidden md:block md:col-span-2"></div>
-
-              <div className="flex gap-3 md:col-span-5 md:flex-row sm: flex-col">
-                <p className="m-0 text-base whitespace-nowrap">Получатель:</p>
-                <div className="flex items-center gap-2 pl-[10px]">
-                  <img src={icUser} alt="User icon" className="w-5 h-5"/>
-                  <p className="m-0 font-bold text-base">{recipient.fullName}</p>
-                </div>
+            <div className="flex justify-between md:justify-end gap-4 md:col-span-5 md:flex-row sm: flex-col">
+              <div className="flex items-center gap-2 ">
+                <img src={icPhone} alt="Phone icon" className="w-5 h-5" />
+                <p className="m-0 font-bold text-base md:text-lg">{recipient.phoneNumber}</p>
               </div>
 
-              <div className="flex justify-between md:justify-end gap-4 md:col-span-5 md:flex-row sm: flex-col ml-7">
-                <div className="flex items-center gap-2 ">
-                  <img src={icPhone} alt="Phone icon" className="w-5 h-5"/>
-                  <p className="m-0 font-bold text-base md:text-lg">{recipient.phoneNumber}</p>
+              <button
+                onClick={() => {
+                  navigate(`/parcels/${_id}`);
+                }}
+                className="p-2 rounded-lg hover:bg-amber-100 transition-colors cursor-pointer duration-300 self-start relative group"
+              >
+                <Ellipsis color="#6b6b6b" strokeWidth={3} size={24} />
+
+                <div className="absolute bottom-full right-0 mb-2 px-3 py-1 bg-gray-800 text-white text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap pointer-events-none">
+                  Подробнее
                 </div>
-
-                <button
-                    onClick={() => {
-                      navigate(`/parcels/${_id}`);
-                    }}
-                    className="p-2 rounded-lg hover:bg-amber-100 transition-colors cursor-pointer duration-300 self-start relative group"
-                >
-                  <Ellipsis color="#6b6b6b" strokeWidth={3} size={24}/>
-
-                  <div className="absolute bottom-full right-0 mb-2 px-3 py-1 bg-gray-800 text-white text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap pointer-events-none">
-                    Подробнее
-                  </div>
-                </button>
-              </div>
+              </button>
             </div>
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
