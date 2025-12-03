7b8f449f075552c257e66fd6762c87a5bcde0068
Sadykov Arsen <LoCo10291998@gmail.com>
2025-12-03 18:07:18 +0600

    Commit pull

--- front/src/features/adminPanel/components/AdminToolbar.tsx
+++ front/src/features/adminPanel/components/AdminToolbar.tsx
@@ -80,9 +80,9 @@ export const AdminToolbar = () => {
   const isSuperAdmin = admin?.role === 'superAdmin';
 
   return (
-    <NavigationMenu className="py-3 md:py-4 [&>div]:w-full container">
-      <NavigationMenuList
-        className="
+      <NavigationMenu className="py-3 md:py-4 [&>div]:w-full container">
+        <NavigationMenuList
+            className="
         grid
         w-full
         gap-x-5
@@ -95,78 +95,78 @@ export const AdminToolbar = () => {
         lg:grid-cols-[auto_1fr_auto]
         lg:grid-rows-1
   "
-      >
-        <NavigationMenuItem className="flex justify-end col-start-1 row-start-1">
-          <Link to={'/admin'}>
-            <img src={logoImage} alt="logo" className="w-30 md:w-25" />
-          </Link>
-        </NavigationMenuItem>
+        >
+          <NavigationMenuItem className="flex justify-end col-start-1 row-start-1">
+            <Link to={'/admin'}>
+              <img src={logoImage} alt="logo" className="w-30 md:w-25" />
+            </Link>
+          </NavigationMenuItem>
 
-        <div className="flex flex-col md:flex-row gap-3 justify-center items-stretch w-full col-start-1 col-end-3 row-start-2 lg:col-start-2 lg:col-end-2 lg:row-start-1">
-          <NavigationMenuItem className="flex-1">
-            <Input
-              type="search"
-              name="trackingNumber"
-              id="trackingNumber"
-              placeholder="Трек номер посылки"
-              value={search.trackingNumber}
-              onChange={inputChangeHandler}
-              className="focus-visible:border-amber-600 focus-visible:ring-amber-600 focus-visible:ring-1 w-full"
-            />
-          </NavigationMenuItem>
-          <NavigationMenuItem className="flex-1">
-            <Input
-              type="search"
-              name="sender"
-              id="sender"
-              placeholder="ФИО отправителя"
-              value={search.sender}
-              onChange={inputChangeHandler}
-              className="focus-visible:border-amber-600 focus-visible:ring-amber-600 focus-visible:ring-1 w-full"
-            />
-          </NavigationMenuItem>
-          <NavigationMenuItem className="flex-1">
-            <Input
-              type="search"
-              name="recipient"
-              id="recipient"
-              placeholder="ФИО получателя"
-              value={search.recipient}
-              onChange={inputChangeHandler}
-              className="focus-visible:border-amber-600 focus-visible:ring-amber-600 focus-visible:ring-1 w-full"
-            />
-          </NavigationMenuItem>
-        </div>
+          <div className="flex flex-col md:flex-row gap-3 justify-center items-stretch w-full col-start-1 col-end-3 row-start-2 lg:col-start-2 lg:col-end-2 lg:row-start-1">
+            <NavigationMenuItem className="flex-1">
+              <Input
+                  type="search"
+                  name="trackingNumber"
+                  id="trackingNumber"
+                  placeholder="Трек номер посылки"
+                  value={search.trackingNumber}
+                  onChange={inputChangeHandler}
+                  className="focus-visible:border-amber-600 focus-visible:ring-amber-600 focus-visible:ring-1 w-full"
+              />
+            </NavigationMenuItem>
+            <NavigationMenuItem className="flex-1">
+              <Input
+                  type="search"
+                  name="sender"
+                  id="sender"
+                  placeholder="ФИО отправителя"
+                  value={search.sender}
+                  onChange={inputChangeHandler}
+                  className="focus-visible:border-amber-600 focus-visible:ring-amber-600 focus-visible:ring-1 w-full"
+              />
+            </NavigationMenuItem>
+            <NavigationMenuItem className="flex-1">
+              <Input
+                  type="search"
+                  name="recipient"
+                  id="recipient"
+                  placeholder="ФИО получателя"
+                  value={search.recipient}
+                  onChange={inputChangeHandler}
+                  className="focus-visible:border-amber-600 focus-visible:ring-amber-600 focus-visible:ring-1 w-full"
+              />
+            </NavigationMenuItem>
+          </div>
 
-        <NavigationMenuItem className="flex gap-3 justify-start col-start-2 lg:col-start-3 lg:row-start-1">
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button className="bg-brand hover:bg-amber-600 transition duration-300 active:bg-amber-700">
-                Меню
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-48" align="end">
-              <DropdownMenuGroup>
-                {!isSuperAdmin ? null : (
-                  <>
-                    <Link to="moderation">
-                      <DropdownMenuItem>Администраторы</DropdownMenuItem>
-                    </Link>
-                    <DropdownMenuItem
-                      onSelect={(e) => e.preventDefault()}
-                      className="hidden sm:flex "
-                    >
-                      <ModalFile />
-                    </DropdownMenuItem>
-                  </>
-                )}
-                <DropdownMenuItem onClick={async () => await logout()}>Выйти</DropdownMenuItem>
-              </DropdownMenuGroup>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+          <NavigationMenuItem className="flex gap-3 justify-start col-start-2 lg:col-start-3 lg:row-start-1">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button className="bg-brand hover:bg-amber-600 transition duration-300 active:bg-amber-700">
+                  Меню
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-48" align="end">
+                <DropdownMenuGroup>
+                  {!isSuperAdmin ? null : (
+                      <>
+                        <Link to="moderation">
+                          <DropdownMenuItem>Администраторы</DropdownMenuItem>
+                        </Link>
+                        <DropdownMenuItem
+                            onSelect={(e) => e.preventDefault()}
+                            className="hidden sm:flex "
+                        >
+                          <ModalFile />
+                        </DropdownMenuItem>
+                      </>
+                  )}
+                  <DropdownMenuItem onClick={async () => await logout()}>Выйти</DropdownMenuItem>
+                </DropdownMenuGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
   );
 };
 
